d88e16645f8940b9c32b79a2ccc8c68728a5c8e0
Rodrigo Silveira Mori <RodrigoSilveiraMori@hotmail.com>
2021-12-15 09:43:57 -0300

    Update EXTD1.cpp

--- C/EXTD1.cpp
+++ C/EXTD1.cpp
@@ -9,11 +9,11 @@
     ERROPOSIÇÃO = -3
 
     */
-    #define maxlista 100
-    #define ErroListaCheia -1
-    #define ErroListaVazia -2
-    #define ErroPosicao -3
-    #define Verdadeiro -4
+    #define maxlista 8
+    #define ErroListaCheia -2
+    #define ErroListaVazia -3
+    #define ErroPosicao -4
+    #define Verdadeiro -5
     #define Falso 0
     listaInteiros aLista;
 
@@ -60,17 +60,58 @@
             return aLista.ultimo;
 
              if (adicionaLista (20)>=0)
-            printf ("\n elemento adicionado");
+                printf ("\n elemento adicionado");
             else
-            printf ("\n elemento nao adicionado");
+                printf ("\n elemento nao adicionado");
         }
     }
+
+      int adicionaListanoInicio (int add)
+    {
+        if (listaCheia () == Verdadeiro)
+            return ErroListaCheia;
+        else
+        {
+            int cont;
+            aLista.ultimo++;
+            for(cont = aLista.ultimo; cont >0; cont --)
+            {
+                aLista.dados[cont] = aLista.dados[cont-1];
+            }
+            aLista.dados[cont] = add;
+            return aLista.ultimo;
+        }
+    }
+
+    int retiraLista ()
+    {
+        if(listaVazia()== Verdadeiro)
+            return ErroListaVazia;
+        else{
+            aLista.ultimo--;
+            return aLista.ultimo;
+        }
+    }
+
+    void mostraLista ()
+    {
+        if(listaVazia () == Verdadeiro)
+            printf("\nLista vazia");
+        
+        else{
+            int cont;
+            for (cont=0;cont<=aLista.ultimo;cont++)
+            {
+                printf ("\n aLista [%d]",cont, aLista.dados [cont]);
+            }
+        }
+    }
+
     //-----------------------------------------MAIN-------------------------------------------------
     int main (){
         inicializaLista ();
-        adicionaLista (20);
+        mostraLista ();
         
-        destroiLista ();
 
         destroiLista();
         return 0;
@@ -78,4 +119,4 @@
 
 
 
-// 1:37m
+// 0:45 29nov
