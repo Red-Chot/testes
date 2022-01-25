55d0f748263113078bab17496163ad22d49a3fb5
Red-Chot <86813124+Red-Chot@users.noreply.github.com>
2022-01-25 15:09:21 -0300

    att

---
+++ C/dinamica.h
@@ -0,0 +1,15 @@
+#include <stddef.h>
+
+
+typedef struct lista
+{
+    int info; 
+    struct lista *prox;
+} Lista;
+
+
+Lista* criaLista(){
+    return NULL;
+}
+
+Lista * insereListao
---
+++ C/dinamicamain.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "dinamica.h"
+
+int main ()
+{
+    Lista*aLista;
+    aLista = criaLista();
+    return 0; 
+}
