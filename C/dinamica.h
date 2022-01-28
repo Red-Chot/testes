278bcb37c97f6f5c514de4db80df2c600828ca45
Red-Chot <86813124+Red-Chot@users.noreply.github.com>
2022-01-28 20:07:57 -0300

    att

--- C/dinamica.h
+++ C/dinamica.h
@@ -34,31 +34,44 @@ void imprimeLista (Lista *l)
     {
     
     Lista *t;
-    for (t = l; t!=NULL ;t= t->prox)
-    {
-        printf ("\n Info %d", t->info);
-    }
+        for (t = l; t!=NULL ;t= t->prox)
+        {
+            printf ("\n Info %d", t->info);
+        }
     }
 }
 
 Lista* BuscaLista (Lista *l, int v)
 {   
     if (ListaVazia(l))
-    printf("Lista vazia!\n");
+        printf("Lista vazia!\n");
     else
     {
     
-    Lista *t;
-    for (t = l; t!=NULL ;t= t->prox)
-    {
-        if(t->info == v)
-        {        
-        printf("\n%d foi encontrado",t->info);
-        return t;
+        Lista *t;
+            for (t = l; t!=NULL ;t= t->prox)
+            {
+                if(t->info == v)
+            {        
+            printf("\n%d foi encontrado",t->info);
+            return t;
         }
     }
-    printf("Elemento nao encontrado\n");
+        printf("Elemento nao encontrado\n");
     }
     return NULL;
 }
 
+void removeListaPrimeiro(Lista *l)
+{
+    if (ListaVazia(l))
+    printf("Lista vazia!, não pode ser removido\n");
+    else
+    {
+        Lista *t;
+        t=l;
+        l = l->prox;
+        free(t);
+
+    }
+}
