83d0586ea12a9ab87752160ad687372e538ebb98
Red-Chot <86813124+Red-Chot@users.noreply.github.com>
2022-03-04 09:56:28 -0300

    att

--- C/lista dinamica/arvo.h
+++ C/lista dinamica/arvo.h
@@ -17,3 +17,64 @@ struct node
 };
 
 typedef node Node;
+
+Node *inicializa()
+{
+    return NULL;
+}
+
+Item create(int cod)
+{
+    Item item;
+    item.cod = cod;
+    return item;
+}
+
+Node *inserir(Node *raiz, Item x)
+{   
+    int resp;
+    if (raiz == NULL)
+    {
+        Node*aux =(Node*)malloc(sizeof(Node));
+        aux ->item = x;
+        aux->left = NULL;
+        aux->right= NULL;
+        return aux;
+    }
+    else
+    {   
+        printf("\nDeseja adicionar a direita ou esquerda? 1/DIREITA | 2/ESQUERDA\n");
+        scanf ("%d",&resp);
+        if (resp == 1)
+        {
+            raiz->right =inserir(raiz->right,x);
+        }
+        else if (resp == 2)
+        {
+            raiz->left =inserir(raiz->left,x);
+        }
+        
+
+    }
+    return raiz;
+}
+
+void imprimir(Node *raiz)
+{
+    if(raiz != NULL)
+    {
+        printf("%d ",raiz->item.cod);
+        imprimir(raiz->left);
+        imprimir(raiz->right);
+    }
+}
+
+void libera(Node *raiz)
+{
+    if(raiz != NULL)
+    {
+        libera(raiz->left);
+        libera(raiz->right);
+        free(raiz);
+    }
+}
--- C/lista dinamica/arvo.main.cpp
+++ C/lista dinamica/arvo.main.cpp
@@ -2,10 +2,17 @@
 #include <stdlib.h>
 #include "arvo.h"
 
+
 int main()
 {
-    Node *raiz = NULL;
-
+    Node *raiz = inicializa();
+    raiz=inserir(raiz,create(10));
+    raiz=inserir(raiz,create(25));
+    raiz=inserir(raiz,create(14));
+    imprimir(raiz);
+    printf("\n");
+    libera(raiz);
 
     return 0;
 }
+
